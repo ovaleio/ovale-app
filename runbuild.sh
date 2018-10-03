3ee336e08241ea5d5484cce7d184d9e980a64c0e
slashbinslashnoname <slashbinslashnoname@gmail.com>
2018-10-03 15:42:29 +0400

    Run Build script added

---
+++ runbuild.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+rm -rf node_modules/
+rm -rf dist
+rm -rf ~/Library/Caches/electron
+rm -rf ~/Library/Caches/electron-builder
+rm -rf ~/Library/Saved\ Application\ State/com.github.electron.savedState
+npm install
